20b166395cb38dd70b10c0d73e6a1a8becbdec05
bkepps <59382763+bkepps@users.noreply.github.com>
2020-07-12 09:41:47 -0500

    Slider is more seperate in preparation for adding it to other project

---
+++ graphicsTest/Slider.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <SDL.h>
+/*
+struct with all key elements of a slide selector
+*/
+typedef struct {
+	Uint8 position;
+	Uint8 numberOfPositions;
+	Uint16 spaceBetweenPositions;
+	SDL_Rect slideRailRectangle;			//
+	SDL_Rect sliderArrowRectangle;			//
+	Uint8 move;								//BOOL
+} Slider;
+
+int Slider_MoveWithMouse(SDL_Point, Slider*);
+
+int Slider_Render(SDL_Renderer*, SDL_Texture*, SDL_Texture*, Slider*);
